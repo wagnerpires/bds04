c9347f70ead0cdc5bbf32342fc4ca0c2db9f9969
wagnerpires@hotmail.com <wagnerpires@hotmail.com>
2021-06-07 12:40:42 -0300

    Pequena alteração TOC

--- src/main/java/com/devsuperior/bds04/dto/EventDTO.java
+++ src/main/java/com/devsuperior/bds04/dto/EventDTO.java
@@ -10,80 +10,79 @@ import javax.validation.constraints.NotNull;
 import com.devsuperior.bds04.entities.Event;
 
 public class EventDTO implements Serializable {
-	private static final long serialVersionUID = 1L;
-	
-private Long id;
-	
-	
-	@NotBlank(message = "Campo requerido")
-	private String name;
-	
-	@FutureOrPresent(message = "A data do evento não pode ser passada")
-	private LocalDate date;
-	
-	private String url;
-	
-	@NotNull(message = "Campo requerido")
-	private Long cityId;
-	
-/*
-	public EventDTO() {
-		
-	}
-*/	
-	public EventDTO(Long id, String name, LocalDate date, String url, Long cityId) {
-		this.id = id;
-		this.name = name;
-		this.date = date;
-		this.url = url;
-		this.cityId = cityId;
-	}
+    private static final long serialVersionUID = 1L;
 
-	public EventDTO(Event entity) {
-		id = entity.getId();
-		name = entity.getName();
-		date = entity.getDate();
-		url = entity.getUrl();
-		cityId = entity.getCity().getId();
-	}
-	
-	public Long getId() {
-		return id;
-	}
+    private Long id;
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    @NotBlank(message = "Campo requerido")
+    private String name;
 
-	public String getName() {
-		return name;
-	}
+    @FutureOrPresent(message = "A data do evento não pode ser passada")
+    private LocalDate date;
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    private String url;
 
-	public LocalDate getDate() {
-		return date;
-	}
+    @NotNull(message = "Campo requerido")
+    private Long cityId;
 
-	public void setDate(LocalDate date) {
-		this.date = date;
-	}
+    /*
+        public EventDTO() {
 
-	public String getUrl() {
-		return url;
-	}
+        }
+    */
+    public EventDTO(Long id, String name, LocalDate date, String url, Long cityId) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.url = url;
+        this.cityId = cityId;
+    }
 
-	public void setUrl(String url) {
-		this.url = url;
-	}
+    public EventDTO(Event entity) {
+        id = entity.getId();
+        name = entity.getName();
+        date = entity.getDate();
+        url = entity.getUrl();
+        cityId = entity.getCity().getId();
+    }
 
-	public Long getCityId() {
-		return cityId;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void setCityId(Long cityId) {
-		this.cityId = cityId;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Long getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(Long cityId) {
+        this.cityId = cityId;
+    }
 }
